bc9bc6c0f9cbd0779eb3c9dd4779e0a31359c50d
Raymundo Angel Vazquez Rojas <ray.vazquez48@gmail.com>
2020-07-31 14:35:17 -0500

    Inicio de proyecto

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PaginaWebMasicsa
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
