e0c8dfadba3cd3808d42f78f901cb2437a1e043b
Raymundo Angel Vazquez Rojas <ray.vazquez48@gmail.com>
2020-08-01 11:50:41 -0500

    Corrigiendo errores
    Se eliminaron archivos innecesarios

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,17 +8,6 @@ namespace PaginaWebMasicsa
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                           "~/Content/lib/jquery/jquery.min.js",
                           "~/Content/lib/bootstrap/js/bootstrap.min.js",
